a56ec666992b771ff40c60cd3e391a5281b286cb
vanthanh <nguyenthivan18012000@gmail.comm>
2022-02-20 21:06:53 +0700

    fix bug

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const jsonServer = require("json-server-auth");
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+server.db = router.db
+
+server.use(auth);
 server.use(middlewares);
 server.use(router);
 
