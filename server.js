b79c1cb3bafaee14ab5ca8abf39f3303370e39d0
vanthanh <nguyenthivan18012000@gmail.comm>
2022-02-20 22:46:32 +0700

    fix

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.db = router.db
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
 
 server.use(auth);
 server.use(middlewares);
