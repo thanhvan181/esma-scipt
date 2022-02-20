cd5bffb7625a00af68824a11241aa25b13e90881
vanthanh <nguyenthivan18012000@gmail.comm>
2022-02-20 18:16:30 +0700

    refactor lan2

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosClient = axios.create({
-    baseURL: '192.46.226.178',
+    baseURL: 'http://192.46.226.178',
     headers: {
         'Content-Type': 'application/json'
     }
